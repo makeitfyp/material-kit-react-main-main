f8f7017fa6957fe740adc3e50631cc9805422fa3
Salmanki-Hub <ms953864@gmail.com>
2021-12-07 21:49:17 +0500

    recipes list updated

--- src/__mocks__/recipe.js
+++ src/__mocks__/recipe.js
@@ -3,70 +3,70 @@ import { v4 as uuid } from 'uuid';
 export default [
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_3.png',
-    title: 'ekaterina.tankova@devias.io',
+    avatarUrl: '/static/images/recipes/biryani.jpg',
+    title: 'Chicken Biryani',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_4.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/pizza.jpg',
+    title: 'Chicken Qorma',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_2.png',
-    title: 'cao.yu@devias.io',
-    ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
-
-  },
-  {
-    id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_5.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/daal chawal.jpg',
+    title: 'Daal Chawal',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
 
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_6.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/biryani.jpg',
+    title: 'BBQ Chicken',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
 
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_1.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/pizza.jpg',
+    title: 'BBQ Pizza',
+    ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
+
+  },
+  {
+    id: uuid(),
+    avatarUrl: '/static/images/recipes/biryani.jpg',
+    title: 'Saashilay',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
 
   },
   {
     id: uuid(),
 
-    avatarUrl: '/static/images/avatars/avatar_7.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/biryani.jpg',
+    title: 'Aloo Qeema',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
 
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_8.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/biryani.jpg',
+    title: 'Aloo Matar Curry',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
 
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_9.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/kareela.jpg',
+    title: 'Kareela Salan',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
 
   },
   {
     id: uuid(),
-    avatarUrl: '/static/images/avatars/avatar_10.png',
-    title: 'cao.yu@devias.io',
+    avatarUrl: '/static/images/recipes/daal mash.jpg',
+    title: 'Daal Maash',
     ingredients: '1 cup boiled basmati rice, 1/2 teaspoon mint leaves salt as required, 2 tablespoon refined oil, 3 green cardamom, 2 clove, 2 onion, 1 teaspoon turmeric, 1 tablespoon garlic paste, 1 cup hung curd, 2 tablespoon coriander leaves water as required, 1 tablespoon ghee, 600 gm chicken, 1 tablespoon garam masala powder, 1 teaspoon saffron, 1 tablespoon bay leaf, 1 black cardamom, 1 teaspoon cumin seeds, 4 green chillies, 1 tablespoon ginger paste, 1 teaspoon red chilli powder, 1/2 tablespoon ginger, 2 drops kewra, 1 tablespoon rose water'
   }
 ];
